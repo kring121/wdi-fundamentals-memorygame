ef08ce811d72303db747149ec36d8e6ebae31d26
Christian von Lehe <christianvonlehe@Christians-MacBook-Air.local>
2018-07-27 18:48:43 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -31,15 +31,34 @@ alert("You found a match!");
 alert("Sorry, try again.");
 }
 }
-}
+};
 
-var flipCard = function (cardId) {
+var flipCard = function () {
+  var cardId = this.getAttribute('data-id')
+  this.setAttribute('src', cards[cardId].cardImage);
   console.log("User flipped " + cards[cardId].rank);
   console.log(cards[cardId].cardImage);
   console.log(cards[cardId].suit);
   cardsInPlay.push(cards[cardId].rank);
   checkForMatch();
-}
+};
 
-flipCard(2);
-flipCard(3);
+
+var createBoard = function () {
+  for (var i = 0; i < cards.length; i ++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', "images/back.png");
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+  }
+};
+
+var resetBoard = function () {
+ window.location.reload();
+};
+
+createBoard();
+
+var button = document.getElementById('reset');
+button.addEventListener('click', resetBoard);
